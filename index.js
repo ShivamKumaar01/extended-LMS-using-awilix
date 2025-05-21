13ab7dd98d9aaaadc9743416969fb8630cb6855d
shivam <shivamkumaar01@gmail.com>
2025-05-21 12:05:33 +0530

    login signup is in working

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-
+'use strict'
+const ps = require('prompt-sync')
+const prompt = ps({ sigint: true })
 const container = require('./container');
 const library = container.resolve('library');
 
-library.addBook('abc', 'def', 'regular');
-library.addBook('Advanced Node.js', 'Jane Smith', 'premium');
-library.addBook('Global Algorithms', 'Alan Turing', 'global');
+// library.addBook('abc', 'def', 'regular');
+// library.addBook('Advanced Node.js', 'Jane Smith', 'premium');
+// library.addBook('Global Algorithms', 'Alan Turing', 'global');
 
-library.displayBooks('regular');
-console.log('hellow')
+// library.displayBooks('regular');
+console.log("Welcome in library managment system")
+let isStart=true;
+while(isStart){
+    console.log("press r if u  are new here");
+    console.log("press l if u want to login");
+    const value=prompt();
+    console.log(value);;
+}
